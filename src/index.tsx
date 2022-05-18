e8d33500bca853ca0484f62509ff1ca27ef9f713
Leandro Viana Silva <leandroviana.vs@gmail.com>
2022-05-18 14:45:32 -0300

    desafio concluido com a componetizacao do app

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import { render } from 'react-dom'
 
 import { App } from './App'
 
-render(<App />, document.getElementById('root'))
+render(<App id={0} name={'action'} title={''} />, document.getElementById('root'))
